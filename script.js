25aaac09971b42a70906b9ee1b8bc3a30f8775f6
Ruan Chagas <ruanchagas95@gmail.com>
2023-05-18 04:03:33 -0300

    adjust rules and new songs

--- script.js
+++ script.js
@@ -1,70 +1,70 @@
 const mainHtml = document.querySelector('main')
 const html = document.querySelector('html')
 html.addEventListener('keydown', main)
-let word = prompt('Digite o nome da criança(sem acentos):');
+let word = prompt('Digite o nome da criança(sem acentos):')
 word = word.toUpperCase()
 let wordIndex = 0
-writing = document.querySelector('#writing')
-hintKey = document.querySelector('#hintKey')
-audios = {}
+const writing = document.querySelector('#writing')
+const hintKey = document.querySelector('#hintKey')
+const audios = {}
 
 function main(event) {
-    const key = event.key
-    if (key.toUpperCase() === word[wordIndex]) {
-        mainHtml.style.backgroundColor = mainHtml.style.backgroundColor === 'red' ? 'yellow' : 'red';
-        if (wordIndex < word.length - 1){
-            wordIndex += 1
-            updateHintKey()
-            updateWriting()
-            hintKey.style.fontSize = '200%'
-            hintKey.classList.remove('attention')
-            playAudio('hit')
-        } else{
-            winner()
-            
-        }
+  const key = event.key
+  if (key.toUpperCase() === word[wordIndex]) {
+    mainHtml.style.backgroundColor = mainHtml.style.backgroundColor === 'red' ? 'yellow' : 'red'
+    if (wordIndex < word.length - 1){
+      wordIndex += 1
+      updateHintKey()
+      updateWriting()
+      hintKey.style.fontSize = '200%'
+      hintKey.classList.remove('attention')
+      playAudio('hit')
     } else{
-        upSizeIntruction()
-        hintKey.classList.add('attention')
+      winner()
+            
     }
+  } else{
+    upSizeIntruction()
+    hintKey.classList.add('attention')
+  }
     
 }
 function updateHintKey(){
-    hintKey.innerText = word[wordIndex]
+  hintKey.innerText = word[wordIndex]
 }
 function updateWriting(){
-    let writingText = ''
-    for (let index = 0; index < wordIndex; index++){
-        writingText += word[index]
-    }
-    writing.innerText = writingText
+  let writingText = ''
+  for (let index = 0; index < wordIndex; index++){
+    writingText += word[index]
+  }
+  writing.innerText = writingText
 }
 function upSizeIntruction(){
-    fontSize = parseInt(hintKey.style.fontSize)
-    if (fontSize < 250 || isNaN(fontSize)){
-        hintKey.style.fontSize = fontSize ? `${fontSize + 10}%` : '210%'
-    }
+  const fontSize = parseInt(hintKey.style.fontSize)
+  if (fontSize < 250 || isNaN(fontSize)){
+    hintKey.style.fontSize = fontSize ? `${fontSize + 10}%` : '210%'
+  }
 }
 function loadAudios(){
-audios['hit'] = new Audio('songs/hit.mp3')
-audios['hit'].volume = 0.2;
-audios['winner'] = new Audio('songs/winner.wav')
+  audios['hit'] = new Audio('songs/hit.mp3')
+  audios['hit'].volume = 0.2
+  audios['winner'] = new Audio('songs/winner.wav')
 }
 function playAudio(audio){
-    if (audios[audio]){
-        audios[audio].currentTime = 0;        
-        audios[audio].play();
-    }
+  if (audios[audio]){
+    audios[audio].currentTime = 0        
+    audios[audio].play()
+  }
 }
 
 function winner(){
-    console.log('winner');
-    playAudio('winner')
-    wordIndex = 0
-    hintKey.classList.remove('attention')
-    hintKey.style.fontSize = '200%'
-    updateHintKey()
-    updateWriting()
+  console.log('winner')
+  playAudio('winner')
+  wordIndex = 0
+  hintKey.classList.remove('attention')
+  hintKey.style.fontSize = '200%'
+  updateHintKey()
+  updateWriting()  
 }
 loadAudios()
 updateHintKey()
